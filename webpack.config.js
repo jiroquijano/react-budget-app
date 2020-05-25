a5e7153b184fa392073bdc70303b02db80698644
quijano <safiro.quijano@nokia.com>
2020-05-25 22:04:19 +0800

    fixed webpack dev server options to enable live loading

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,9 @@ const path = require('path');
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname,'public/scripts'),
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'public'),
+        filename: 'scripts/bundle.js'
     },
-    mode: 'development',
     module:{
         rules:[{
             loader: 'babel-loader',
@@ -24,7 +23,8 @@ module.exports = {
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname,'public'),
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true,
         watchContentBase: true
     }
 };
