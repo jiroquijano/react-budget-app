cc95aeedcabb8d8c8931db3bf48d4569a7d34985
quijano <safiro.quijano@nokia.com>
2020-05-25 22:26:50 +0800

    Used BrowserRouter and Route components from 'react-router-dom' module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
-const appRoot = document.querySelector('#app');
-ReactDOM.render(<h1>Hello world!</h1>,appRoot);
+const ExpenseDashboardPage = ()=>(
+    <div>
+        This is from the jiro's dashboard component
+    </div>
+);
+
+const AddExpensePage = ()=>(
+    <div>
+        expense page
+    </div>
+);
+
+const EditExpensePage = ()=>(
+    <div>
+        Edit Expense Page
+    </div>
+);
+
+const HelpPage = (props) =>(
+    <div>
+        Help Me
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" exact={true} component={ExpenseDashboardPage}/>
+            <Route path="/expense" component={AddExpensePage}/>
+            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/help" component={HelpPage}/> 
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes,document.querySelector('#app'));
