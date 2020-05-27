a63288c8897e3f834e49de686ddc940c83edb25f
quijano <jiroquijano@gmail.com>
2020-05-27 12:14:55 +0800

    edited header h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 
 const Header = () => (
     <header>
-        <h1>Expensify</h1>
+        <h1>Expensify App</h1>
         <NavLink activeClassName="is-active" exact to="/">HOME | </NavLink>
         <NavLink activeClassName="is-active" exact to="/create"> ADD EXPENSE | </NavLink>
         <NavLink activeClassName="is-active" exact to="/help"> HELP | </NavLink>
