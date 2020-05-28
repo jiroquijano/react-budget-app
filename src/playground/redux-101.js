4dbee52c355ca762780ddd477395ff5d0192b830
quijano <jiroquijano@gmail.com>
2020-05-28 20:34:04 +0800

    Look at this, it's beautiful

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,25 +1,41 @@
 import {createStore} from 'redux';
 
+const incrementCount = ({incrementBy = 1}={})=>({
+    type:'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({decrementBy = 1} = {}) =>({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const resetCount = ()=>({
+    type: 'RESET'
+});
+
+const setCount = ({count = 0}={})=>({
+    type: 'SET',
+    count : typeof count === 'number' ? count : 0
+});
+
 const store = createStore((state = {count:0}, action)=>{
     switch (action.type){
         case 'INCREMENT': 
-            const isIncrementNumber = (typeof action.incrementBy === 'number');
-            const isDecrementNumber = (typeof action.decrementBy === 'number');
-            
             return{
-                count: state.count +  isIncrementNumber ? action.incrementBy : 1
-            };
+                count: state.count + action.incrementBy
+            }
         case 'DECREMENT':
             return{
-                count: state.count - isDecrementNumber ? action.decrementBy : 1
-            }
-        case 'RESET':
-            return {
-                count: 0
+                count: state.count - action.decrementBy
             }
         case 'SET' :
             return {
                 count: action.count
+            }
+        case 'RESET':
+            return {
+                count: 0
             }
         default: 
             return state;
@@ -30,20 +46,10 @@ const unsubscribe = store.subscribe((some)=>{
 
 })
 
-store.dispatch({
-    type:'INCREMENT',
-    incrementBy: 5
-});
-
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 2
-});
-
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
-
+store.dispatch(incrementCount({incrementBy: 10}));
+store.dispatch(decrementCount({decrementBy: 5}));
+store.dispatch(setCount({count:100}));
+store.dispatch(resetCount());
 console.log(store.getState());
+
 
