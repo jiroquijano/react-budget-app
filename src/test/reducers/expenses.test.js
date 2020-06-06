94e1d85c4e69fe80be08e87e8c2658c61966f239
quijano <jiroquijano@gmail.com>
2020-06-06 17:57:26 +0800

    Created expenses fixture and used it to expense reducer and expense selector tests

--- src/test/reducers/expenses.test.js
+++ src/test/reducers/expenses.test.js
@@ -1,5 +1,6 @@
 import expensesReducer from '../../reducers/expenses';
 import moment from 'moment';
+import expensesFixture from '../fixtures/expenses-fixtures';
 
 test ("should return default state", ()=>{
     const state = expensesReducer(undefined,{type:'@@INIT'});
@@ -8,52 +9,31 @@ test ("should return default state", ()=>{
 
 test ("should be able to add expense",()=>{
     const expense = {
+        id:'4',
         description : 'new expense',
         note:'',
         amount:50,
         createdAt: moment()
     }
-    const state = expensesReducer(undefined,{type:'ADD_EXPENSE',expense});
-    expect(state).toEqual([expense]);
+    const state = expensesReducer(expensesFixture,{type:'ADD_EXPENSE',expense});
+    expect(state).toEqual([...expensesFixture, expense]);
 });
 
 test ("should be able to remove expense",()=>{
-    const currentState = [{
-        id: '1',
-        description : 'new expense',
-        note:'',
-        amount:50,
-        createdAt: moment()
-    }];
-    const state = expensesReducer(currentState,{type: 'REMOVE_EXPENSE', id: '1'});
-    expect(state).toEqual([]);
+    const state = expensesReducer(expensesFixture,{type: 'REMOVE_EXPENSE', id: '1'});
+    expect(state).toEqual(expensesFixture.slice(1));
 });
 
 test ("should not be able to delete non-existing expense id", ()=>{
-    const currentState = [{
-        id: '1',
-        description : 'new expense',
-        note:'',
-        amount:50,
-        createdAt: moment()
-    }];
-    const state = expensesReducer(currentState,{type:'REMOVE_EXPENSE',id:'2'});
-    expect(state).toEqual(currentState);
+    const state = expensesReducer(expensesFixture,{type:'REMOVE_EXPENSE',id:'5'});
+    expect(state).toEqual(expensesFixture);
 });
 
 test ("should be able to edit existing expense", ()=>{
-    const existingItem = {
-        id: '1',
-        description : 'new expense',
-        note:'',
-        amount:50,
-        createdAt: moment()
-    };
-    const currentState = [existingItem];
     const updates = {
         description: 'updated expense',
         amount: 100
     };
-    const state = expensesReducer(currentState, {type:'EDIT_EXPENSE',id: '1', updates})
-    expect(state).toEqual([{...existingItem, ...updates}]);
+    const state = expensesReducer(expensesFixture, {type:'EDIT_EXPENSE',id: '1', updates})
+    expect(state).toEqual([{...state[0], ...updates}, ...expensesFixture.slice(1)]);
 });
--- src/test/selectors/expenses.test.js
+++ src/test/selectors/expenses.test.js
@@ -1,28 +1,6 @@
 import getVisibleExpenses from '../../selectors/expenses';
 import moment from 'moment';
-let expenses = [];
-
-beforeEach(()=>{
-    expenses = [{
-        id: '1',
-        description: 'Gum',
-        note: '',
-        amount: 195,
-        createdAt: 0
-    },{
-        id: '2',
-        description: 'Rent',
-        note: '',
-        amount: 109500,
-        createdAt: moment(0).subtract(4, 'days').valueOf()
-    },{
-        id: '3',
-        description: 'Credit card',
-        note: '',
-        amount: 4500,
-        createdAt: moment(0).add(4, 'days').valueOf()
-    }];
-});
+import expenses from '../fixtures/expenses-fixtures';
 
 test ("should filter by text value", ()=>{
     const filters = {
