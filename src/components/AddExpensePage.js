4d057cd9af5495acb2ca6237d96aeccda6b7e771
quijano <jiroquijano@gmail.com>
2020-06-08 21:41:20 +0800

    Used react-redux feature 'mapDispatchToProps' to make the component more testable

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,16 +3,37 @@ import ExpenseForm from './ExpenseForm';
 import {connect} from 'react-redux';
 import {addExpense} from '../actions/expenses';
 
-const AddExpensePage = (props) =>(
-    <div>
-        <h1>Expense page</h1>
-        <ExpenseForm onSubmit={(expense)=>{
-            props.dispatch(addExpense(expense));
-            props.history.push("/");
-        }}/>
-    </div>
-);
+export class AddExpensePage extends React.Component{
+    onSubmit = (expense) =>{
+        this.props.addExpenseDispatch(expense);
+        this.props.history.push("/");
+    }
 
-const ConnectedAddExpensePage = connect()(AddExpensePage);
+    render(){
+        return (
+            <div>
+                <h1>Expense page</h1>
+                <ExpenseForm onSubmit={this.onSubmit}/>
+            </div>
+        );
+    };
+}
+
+// const AddExpensePage = (props) =>(
+//     <div>
+//         <h1>Expense page</h1>
+//         <ExpenseForm onSubmit={(expense)=>{
+//             //props.dispatch(addExpense(expense)); 
+//             props.onSubmit(expense); //thanks to mapDispatchToProps from react-redux
+//             props.history.push("/");
+//         }}/>
+//     </div>
+// );
+
+const mapDispatchToProps = (dispatch)=>({
+        addExpenseDispatch: (expense) => dispatch(addExpense(expense))
+});
+
+const ConnectedAddExpensePage = connect(undefined,mapDispatchToProps)(AddExpensePage);
 
 export default ConnectedAddExpensePage;
