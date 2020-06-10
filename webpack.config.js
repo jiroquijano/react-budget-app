fd86134492482d12628a5d517875b215b71f98fe
quijano <jiroquijano@gmail.com>
2020-06-10 15:21:19 +0800

    cleaned up generated files from public directories

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env)=>{
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'scripts/bundle.js'
         },
         module:{
@@ -48,6 +48,7 @@ module.exports = (env)=>{
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
+            publicPath: '/dist/',
             historyApiFallback: true,
             watchContentBase: true
         }
