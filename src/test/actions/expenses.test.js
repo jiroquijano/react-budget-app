b40b1f7bd3a3c9be27091af99d139c6e94b07f40
jiroquijano <jiroquijano@gmail.com>
2020-06-17 23:04:41 +0800

    added authentication in firebase structure and updated the related expenses actions

--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -14,6 +14,8 @@ import thunk from 'redux-thunk';
 import database, {firebase} from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
+const uid = 'testuid'
+const defaultAuth = {auth:{uid}}
 
 beforeEach((done)=>{
     const expensesData = {};
@@ -25,13 +27,13 @@ beforeEach((done)=>{
             createdAt
         }
     });
-    database.ref('expenses').set(expensesData).then(()=>{
+    database.ref(`users/${uid}/expenses`).set(expensesData).then(()=>{
         done();
     });
 });
 
 test ("should add expense to database and store", (done)=>{
-    const store = createMockStore({});
+    const store = createMockStore(defaultAuth);
     const expenseData = {
         description: 'Mouse',
         amount: 3000,
@@ -47,7 +49,7 @@ test ("should add expense to database and store", (done)=>{
                 ...expenseData
             }
         });
-        const databasePromise = database.ref(`expenses/${actions[0].expense.id}`).once('value');
+        const databasePromise = database.ref(`users/${uid}/expenses/${actions[0].expense.id}`).once('value');
         databasePromise.then((snapshot)=>{
             expect(snapshot.val()).toEqual(expenseData);
             done();
@@ -57,7 +59,7 @@ test ("should add expense to database and store", (done)=>{
 });
 
 test ("should add expense with defaults to database and store", (done)=>{
-    const store = createMockStore({});
+    const store = createMockStore(defaultAuth);
     store.dispatch(startAddExpense()).then(()=>{
         const actions = store.getActions();
         expect(actions[0]).toEqual({
@@ -114,7 +116,7 @@ test ("should setup setExpense action object with data", ()=>{
 });
 
 test ("should fetch data from database and set the expenses", (done)=>{
-    const store = createMockStore({});
+    const store = createMockStore(defaultAuth);
     const dispatchPromise =  store.dispatch(startSetExpenses());
     dispatchPromise.then(()=>{
         const actions = store.getActions();
@@ -128,7 +130,7 @@ test ("should fetch data from database and set the expenses", (done)=>{
 
 test ("should be able to dispatch remove action",(done)=>{
     const idToRemove = 2;
-    const store = createMockStore(expenses);
+    const store = createMockStore({...defaultAuth,expenses});
     store.dispatch(startRemoveExpense(idToRemove)).then(()=>{
         const actions = store.getActions();
         expect(actions[0]).toEqual({
@@ -140,9 +142,9 @@ test ("should be able to dispatch remove action",(done)=>{
 });
 
 test ("should be able to remove expense from firebase", (done)=>{
-    const store = createMockStore(expenses);
+    const store = createMockStore({...defaultAuth,expenses});
     store.dispatch(startRemoveExpense(2)).then(()=>{
-        database.ref('expenses/2').once('value').then((snapshot)=>{
+        database.ref(`users/${uid}/expenses/2`).once('value').then((snapshot)=>{
             expect(snapshot.val()).toBe(null);
             done();
         });
@@ -154,7 +156,7 @@ test ("should edit expense from firebase", (done)=>{
         description: 'Bubble Gum'
     };
     const {amount, createdAt, description, note} = expenses[0];
-    const store = createMockStore(expenses);
+    const store = createMockStore({...defaultAuth,expenses});
     store.dispatch(startEditExpense(expenses[0].id, updates)).then(()=>{
         const actions = store.getActions();
         expect(actions[0]).toEqual({
@@ -162,7 +164,7 @@ test ("should edit expense from firebase", (done)=>{
             id: expenses[0].id,
             updates
         });
-        database.ref(`expenses/${expenses[0].id}`).once('value').then((snapshot)=>{
+        database.ref(`users/${uid}/expenses/${expenses[0].id}`).once('value').then((snapshot)=>{
             expect(snapshot.val()).toEqual({
                 amount,
                 createdAt,
