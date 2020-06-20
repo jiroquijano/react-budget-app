425d825c876da6bef85eb536ed60dae86afb0868
jiroquijano <jiroquijano@gmail.com>
2020-06-20 14:45:02 +0800

    addes styling on login and header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {startLogout} from '../actions/auth';
 
 export const Header = (props) => (
-    <header>
-        <h1>Expensify App</h1>
-        <NavLink activeClassName="is-active" exact to="/dashboard">DASHBOARD | </NavLink>
-        <NavLink activeClassName="is-active" exact to="/create"> ADD EXPENSE | </NavLink>
-        <NavLink activeClassName="is-active" exact to="/help"> HELP | </NavLink>
-        <button onClick={props.startLogout}> Log out </button>
+    <header className="header">
+        <div className="content-container">
+            <div className="header__content">
+                <Link className="header__title" to="/dashboard">
+                    <h1>Budget app</h1>
+                </Link>
+                <button onClick={props.startLogout}>Log out</button>  
+            </div>
+        </div>
     </header>
         
 );
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import {connect} from 'react-redux';
 import {startLogin} from '../actions/auth';
 
 export const LogInPage = (props) => (
-    <div>
-        <button onClick={props.startLogin}> Login </button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Budget App</h1>
+            <p>it's time to get your budget under control</p>
+            <button className="button" onClick={props.startLogin}> Login with Google </button>
+        </div>
     </div>
 );
 
